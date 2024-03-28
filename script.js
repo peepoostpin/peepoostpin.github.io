570ba83858af6a9a5f11b7ac0c6764d84e6bf713
berrybonk <91695764+peepoostpin@users.noreply.github.com>
2024-03-28 14:59:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,24 @@ function convertMoolahToPounds() {
     const moolahAmount = parseFloat(document.getElementById('moolah').value);
     const exchangeRate = 0.02; // £0.02 = 1 Moolah
     const poundsAmount = moolahAmount * exchangeRate;
-    document.getElementById('result').innerText = `£${poundsAmount.toFixed(2)}`;
+    document.getElementById('resultMoolahToPounds').innerText = `£${poundsAmount.toFixed(2)}`;
+}
+
+function convertPoundsToMoolah() {
+    const poundsAmount = parseFloat(document.getElementById('pounds').value);
+    const exchangeRate = 0.02; // £0.02 = 1 Moolah
+    const moolahAmount = poundsAmount / exchangeRate;
+    document.getElementById('resultPoundsToMoolah').innerText = `${moolahAmount.toFixed(2)} Moolah`;
+}
+
+function multiplyByFactor() {
+    const inputNumber = parseFloat(document.getElementById('multiply').value);
+    const result = inputNumber * 1.25;
+    document.getElementById('multiply').value = result.toFixed(2);
+}
+
+function divideByFactor() {
+    const inputNumber = parseFloat(document.getElementById('divide').value);
+    const result = inputNumber / 2;
+    document.getElementById('divide').value = result.toFixed(2);
 }
